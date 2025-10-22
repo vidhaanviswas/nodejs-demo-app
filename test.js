59c523dcaa763f03591b911808019d698ccf4ca1
Vidhaan Viswas <vidhaanviswas9204@gmail.com>
2025-10-22 10:49:43 +0530

    Fix test to start and close server

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ try {
     console.log('✅ index.js exists — basic test passed.');
     process.exit(0);
   } else {
-    console.error('❌ index.js not found!');
+    console.error('❌ index.js not found in dir!');
     process.exit(1);
   }
 } catch (err) {
